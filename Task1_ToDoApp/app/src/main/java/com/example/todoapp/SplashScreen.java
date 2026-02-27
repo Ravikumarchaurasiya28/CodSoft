1b4152e4b63732206cdcfd3e34b0ab704114b954
Ravikumarchaurasiya28 <ravichaurasiya824@gmail.com>
2026-02-27 20:33:55 +0530

    Added Task1 - ToDoApp

---
+++ Task1_ToDoApp/app/src/main/java/com/example/todoapp/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+
+        getSupportActionBar().hide();
+
+        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
